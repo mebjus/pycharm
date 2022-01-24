ed855bd172baa1905b838b4512e610e5680988db
mebjus <mebjus@gmail.com>
2022-01-24 17:57:27 +0300

    v.3

--- Labs/covid.py
+++ Labs/covid.py
@@ -10,8 +10,27 @@ vaccinations_data = vaccinations_data[
 ]
 
 
-print(covid_data['date'])
-covid_data['date'] = pd.to_datetime(covid_data['date'])
 covid_data = covid_data.groupby(['date', 'country'], as_index=False)[['confirmed', 'deaths', 'recovered']].sum()
 
-print(vaccinations_data)
+covid_data['date'] = pd.to_datetime(covid_data['date'])
+covid_data['active'] = covid_data['confirmed'] - covid_data['deaths'] - covid_data['recovered']
+covid_data = covid_data.sort_values(by=['country', 'date'])
+
+covid_data['daily_confirmed'] = covid_data.groupby('country')['confirmed'].diff()
+covid_data['daily_deaths'] = covid_data.groupby('country')['deaths'].diff()
+covid_data['daily_recovered'] = covid_data.groupby('country')['recovered'].diff()
+
+vaccinations_data['date'] = pd.to_datetime(vaccinations_data['date'])
+
+# print(covid_data['date'].min(), covid_data['date'].max())
+
+covid_df = covid_data.merge(vaccinations_data, on=['date', 'country'], how='left')
+# print(covid_df.shape)
+
+covid_df['death_rate'] = (covid_df['deaths'] / covid_df['confirmed']) * 100
+covid_df['recover_rate'] = (covid_df['recovered'] / covid_df['confirmed']) * 100
+
+c = covid_df[covid_df['country'] == 'United States'].groupby('country')['death_rate'].max()
+# print(round(c, 2))
+russia = covid_df[covid_df['country'] == 'Russia'].groupby('country')['recover_rate'].mean()
+# print(round(russia, 2))
