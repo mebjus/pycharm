01da41df53fd295b9c51b5db2930dd5ac211e879
mebjus <mebjus@gmail.com>
2022-02-15 13:51:53 +0300

    v.1

--- CE/sezon.py
+++ CE/sezon.py
@@ -1,6 +1,11 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 from pandas.api.types import CategoricalDtype
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 df = pd.DataFrame
 dirname = 'data/kis/'
@@ -19,6 +24,18 @@ for file in fullpaths:
             df1 = pd.concat(df1, axis=0).reset_index(drop=True)
             df = pd.concat([df, df1], axis=0)
 
+dirname = 'data/day_of_month.xlsx'
+df_m = pd.read_excel(dirname)
+df_m.reset_index()
+mounth = {}
+
+for i in df_m.index:
+    mounth[df_m.iloc[i]['Дата']] = df_m.iloc[i]['р.д.']
+
+dupl = list(df.columns)
+df_dupl = df[df.duplicated(subset=dupl)]
+df_dupl.index.nunique()
+df = df.drop_duplicates(subset=dupl)
 
 df['Дата Cоздания'] = df['Дата Cоздания'].dt.to_period('M')
 
@@ -51,14 +68,6 @@ df['ФО'] = df['ФО'].astype('category')
 cat_type = CategoricalDtype(categories=['ЦФО', 'СЗФО', 'ПФО', 'ЮФО', 'УФО', 'СФО', 'ДВФО'], ordered=True)
 df['ФО'] = df['ФО'].astype(cat_type)
 
-df['Группа вес'] = pd.cut(df['Расчетный вес'], bins=[0, 0.25, 0.5, 1, 1000000],
-                          labels=['0-0,25', '0,25-0,5', '0,5-1', '1+'], right=False)
-
-df['Группа вес'] = df['Группа вес'].astype('category')
-
-## установить порядок в по весам
-cat_type = CategoricalDtype(categories=['0-0,25', '0,25-0,5', '0,5-1', '1+'], ordered=True)
-df['Группа вес'] = df['Группа вес'].astype(cat_type)
 
 df.rename(columns={'Дата Cоздания': 'дата',
                    'Номер отправления': 'шт', 'Общая стоимость со скидкой': 'деньги', 'Расчетный вес': 'вес'},
@@ -66,26 +75,53 @@ df.rename(columns={'Дата Cоздания': 'дата',
 
 # отбрасываем все нулевки, консолидированные сборы, дешевые доборы
 
-df = df[df['деньги'] > 10]
-# df = df[df['ФО'] == 'СФО']
+df = df[df['деньги'] > 50]
+# df = df[df['ФО'] == 'СЗФО']      ## выбор округа
 
-df_pivot = df.pivot_table(index=['дата'], columns=['Группа вес'], values=['деньги', 'шт', 'вес'],
-                          aggfunc={'деньги': sum, 'шт': len, 'вес': sum})
-
+df_pivot = df.pivot_table(index=['дата'], values=['деньги'],
+                          aggfunc={'деньги': sum})
 
 df_pivot = df_pivot.reset_index()
+df_pivot = df_pivot.reindex(df_pivot.sort_values(by=['дата', 'деньги'], ascending=[True, False]).index).reset_index()
+
+df_pivot['р.д.'] = df_pivot['дата'].apply(lambda x: mounth[str(x)])   ### на рабочий день
+df_pivot['деньги р.д.'] = df_pivot['деньги'] / df_pivot['р.д.']
+
+df_pivot['share'] = (df_pivot['деньги р.д.'] / df_pivot['деньги р.д.'].sum()) * 100
+
+print(df_pivot)
 
 
-print(df_pivot['деньги'])
 
-#######   запись в файл
+writer = pd.ExcelWriter('sezon.xlsx', engine='xlsxwriter')
+df_pivot.to_excel(writer, sheet_name='итоги', startrow=0, index=False, header=False)
 
-# writer = pd.ExcelWriter('0.25.xlsx', engine='xlsxwriter')
-# df_pivot.to_excel(writer, sheet_name='итоги', startrow=0)
-# workbook = writer.book
-# worksheet = writer.sheets['итоги']
-# format1 = workbook.add_format({'border': 1, 'bg_color': '#E8FBE1', 'num_format': '#,##0'})
-# worksheet.set_column('A:N', 10, format1)
-# workbook = writer.book
-# worksheet = writer.sheets['итоги']
-# writer.save()
+workbook = writer.book
+worksheet = writer.sheets['итоги']
+
+format1 = workbook.add_format({'border': 1, 'bg_color': '#E8FBE1', 'num_format': '#,##2'})
+worksheet.set_column('A:B', 10, format1)
+worksheet.set_column('B:C', 65, format1)
+worksheet.set_column('C:I', 15, format1)
+workbook = writer.book
+worksheet = writer.sheets['итоги']
+
+
+writer.save()
+
+######
+
+fig, ax = plt.subplots(figsize=(8, 5))
+plt.xticks(rotation=45)
+# plt.title(name)
+g = sns.barplot(data=df_pivot, x='дата', y='share', color='green')
+# g.axvline(x='2019-12', color='r', lw=2)
+# g.axvline(x='2020-12', color='r', lw=2)
+# g.axvline(x='2021-12', color='r', lw=2)
+ticks_loc = ax.get_yticks().tolist()
+ax.yaxis.set_major_locator(ticker.FixedLocator(ticks_loc))
+ylabels = ['{:,.0f}'.format(x) for x in g.get_yticks()]
+g.set_yticklabels(ylabels)
+
+plt.show()
+#######
