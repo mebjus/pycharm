46288333163606ffd973da363dfdf9a49cbc2bf4
mebjus <mebjus@gmail.com>
2022-02-20 12:17:39 +0300

    v.1

--- CE/billing.py
+++ CE/billing.py
@@ -3,12 +3,69 @@ import pandas as pd
 import os
 from pandas.api.types import CategoricalDtype
 
-
-dirname = 'data/2021-pay.xls'
-df = pd.read_excel(dirname)
+df = pd.DataFrame()
+file = 'data/2021-pay.xls'
+df1 = pd.read_excel(file, header=2, sheet_name=None)
+df1 = pd.concat(df1, axis=0).reset_index(drop=True)
+df = pd.concat([df, df1], axis=0)
 df.reset_index()
+pd.options.display.float_format = '{:,.2F}'.format
+
+df = df[df['Статус оплаты счета'] == 'Оплачен']
+start = df.shape[0]
+
+# df = df[df['Клиент'] == 'ООО «Здоровит»']
+
+def dw(row):
+	if row == 0: d = 'понедельник'
+	elif row == 1: d = 'вторник'
+	elif row == 2: d = 'среда'
+	elif row == 3: d = 'четверг'
+	elif row == 4: d = 'пятница'
+	elif row == 5: d = 'суббота'
+	elif row == 6: d = 'воскресенье'
+	else: d = np.NAN
+	return d
+	
+
+dict_fo = {'СЗФО': ['ВЕЛИКИЙ НОВГОРОД', 'МУРМАНСК', 'ПЕТРОЗАВОДСК', 'СЫКТЫВКАР', 'САНКТ-ПЕТЕРБУРГ', 'АРХАНГЕЛЬСК',
+                    'КАЛИНИНГРАД'],
+           'УФО':  ['КУРГАН', 'НИЖНЕВАРТОВСК', 'НОВЫЙ УРЕНГОЙ', 'СТЕРЛИТАМАК', 'МАГНИТОГОРСК', 'ОРЕНБУРГ', 'СУРГУТ',
+                    'ЕКАТЕРИНБУРГ', 'ПЕРМЬ', 'ТЮМЕНЬ', 'УФА', 'ЧЕЛЯБИНСК'],
+           'ПФО':  ['ИЖЕВСК', 'ПЕНЗА', 'УЛЬЯНОВСК', 'ЧЕБОКСАРЫ', 'КИРОВ', 'НИЖНИЙ НОВГОРОД', 'КАЗАНЬ', 'САМАРА',
+                    'САРАТОВ', 'ТОЛЬЯТТИ'],
+           'ЮФО':  ['НОВОРОССИЙСК', 'СИМФЕРОПОЛЬ', 'ПЯТИГОРСК', 'РОСТОВ-НА-ДОНУ', 'ВОЛГОГРАД', 'ВОРОНЕЖ', 'КРАСНОДАР',
+                    'СТАВРОПОЛЬ', 'АСТРАХАНЬ', 'СОЧИ'],
+           'СФО':  ['БАРНАУЛ', 'НОВОКУЗНЕЦК', 'ТОМСК', 'УЛАН-УДЭ', 'НОВОСИБИРСК', 'КРАСНОЯРСК', 'ОМСК', 'ИРКУТСК',
+                    'КЕМЕРОВО'],
+           'ДВФО': ['ВЛАДИВОСТОК', 'ХАБАРОВСК']
+           }
+
+def ret(cell):  # столбец и ячейку передаю, возрат - округ
+	for i in dict_fo.keys():
+		if str(cell).upper() in dict_fo[i]:
+			return i
+	else:
+		return 'ЦФО'
+
+
+df['ФО'] = df['Заказ.Клиент.Подразделение.Адрес.Город'].apply(ret)
+df['ФО'] = df['ФО'].astype('category')
 
 print(df.info())
+
+df['week'] = df['Оплатить до'].dt.dayofweek
+df['week'] = df['week'].apply(dw)
+
+# print(df['week'].value_counts(normalize=True)*100)
+
+df['delay'] = df['Фактическая дата оплаты'] - df['Оплатить до']
+df = df[df['delay'].dt.components.days > 1]
+finish = df.shape[0]/start
+print('просроченных=', round(finish*100, 2), '%')
+
+lst = ['Статус счета','Статус оплаты счета','week','Сумма с учетом НДС', 'Дата счета', 'Клиент.Отсрочка платежа (дней)']
+df.drop(columns=lst, axis=1, inplace=True)
 
 # for i in df_m.index:
 # 	mounth[df_m.iloc[i]['Дата']] = df_m.iloc[i]['р.д.']
@@ -105,28 +162,24 @@ print(df.info())
 #          inplace=True)
 # print(round((df2['delta_get'].shape[0] / num_start) * 100, 2), '%', 'нарушены сроки сбора')
 #
-# #######
-#
-# writer = pd.ExcelWriter('billing.xlsx', engine='xlsxwriter')
-# df1.to_excel(writer, sheet_name='не доставки', startrow=1, index=False, header=False)
-# df2.to_excel(writer, sheet_name='не сборы', startrow=1, index=False, header=False)
-#
-# workbook = writer.book
-# worksheet = writer.sheets['не доставки']
-# worksheet2 = writer.sheets['не сборы']
-#
-# header_format = workbook.add_format({
-# 	'bold':       True,
-# 	'text_wrap':  True,
-# 	'valign':     'vcenter',
-# 	'fg_color':   '#D7E4BC',
-# 	'align':      'center_across',
-# 	'num_format': '#,##0',
-# 	'border':     1})
-#
-# for col_num, value in enumerate(df1.columns.values):
-# 	worksheet.write(0, col_num, value, header_format)
-# for col_num, value in enumerate(df2.columns.values):
-# 	worksheet2.write(0, col_num, value, header_format)
-#
-# writer.save()
+#######
+
+writer = pd.ExcelWriter('billing.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='оплаты', startrow=1, index=False, header=False)
+
+workbook = writer.book
+worksheet = writer.sheets['оплаты']
+
+header_format = workbook.add_format({
+	'bold':       True,
+	'text_wrap':  True,
+	'valign':     'vcenter',
+	'fg_color':   '#D7E4BC',
+	'align':      'center_across',
+	'num_format': '#,##0',
+	'border':     1})
+
+for col_num, value in enumerate(df.columns.values):
+	worksheet.write(0, col_num, value, header_format)
+
+writer.save()
