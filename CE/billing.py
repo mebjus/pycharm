c2164b2eacd687e76f076a06dd3ba5cee8d4ae7a
mebjus <mebjus@gmail.com>
2022-02-21 13:57:00 +0300

    v.1

--- CE/billing.py
+++ CE/billing.py
@@ -57,27 +57,36 @@ df['Дата счета'] = df['Дата счета'].dt.to_period('M')
 
 ####### фильтры
 
-df = df[df['Дата счета'] == '2021-09']
+# df = df[df['Дата счета'] == '2021-09']
+# df = df[df['ФО'] == 'СЗФО']
 df = df[df['Статус оплаты счета'] == 'Оплачен']
 # df = df[df['Клиент'] == 'ООО «Здоровит»']
 
+df['delay'] = df['Фактическая дата оплаты'] - df['Оплатить до']
+df['delay']  = df['delay'].dt.components.days
+df['delay'] = df['delay'].fillna(0)
+
+
+df['delay'] = df['delay'].apply(lambda x: 0 if x < 0 else x)
 
 start = df.shape[0]
-print(df['week'].value_counts(normalize=True)*100)
+# print(df['week'].value_counts(normalize=True)*100)
+df1 = df.copy()
 
-df['delay'] = df['Фактическая дата оплаты'] - df['Оплатить до']
-df = df[df['delay'].dt.components.days > 1]
+
+df = df[df['delay'] > 1]
 finish = df.shape[0]/start
 print('просроченных=', round(finish*100, 2), '%')
 
 lst = ['Статус счета','Статус оплаты счета','week','Сумма с учетом НДС', 'Клиент.Подразделение.Адрес.Город', 'Дата счета']
 df.drop(columns=lst, axis=1, inplace=True)
+df1.drop(columns=lst, axis=1, inplace=True)
 
 df = df.sort_values(by=['Клиент', 'Оплатить до'], ascending=[True, True])
-
+df1 = df1.sort_values(by=['Клиент', 'Оплатить до'], ascending=[True, True])
 
 writer = pd.ExcelWriter('billing.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='оплаты', startrow=1, index=False, header=False)
+df1.to_excel(writer, sheet_name='оплаты', startrow=1, index=False, header=False)
 
 workbook = writer.book
 worksheet = writer.sheets['оплаты']
@@ -91,7 +100,7 @@ header_format = workbook.add_format({
 	'num_format': '#,##0',
 	'border':     1})
 
-for col_num, value in enumerate(df.columns.values):
+for col_num, value in enumerate(df1.columns.values):
 	worksheet.write(0, col_num, value, header_format)
 
 writer.save()
