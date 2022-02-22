6ccb7c3c387474fe777cd3abb3f8d8ff591cc19a
mebjus <mebjus@gmail.com>
2022-02-22 19:00:05 +0300

    v.1

--- CE/price.py
+++ CE/price.py
@@ -3,8 +3,9 @@ import pandas as pd
 import os
 import requests
 from pprint import pprint
+import pickle
 
-token = ' 25945DB021CBCB00A59775B430B5B8BC'
+token = '25945DB021CBCB00A59775B430B5B8BC'
 
 url = 'https://apitest.cityexpress.ru/v1/25945DB021CBCB00A59775B430B5B8BC/Calculate'
 
@@ -12,52 +13,82 @@ df = pd.DataFrame()
 dirname = 'data/kis/'
 dirfiles = os.listdir(dirname)
 fullpaths = map(lambda name: os.path.join(dirname, name), dirfiles)
-pd.options.display.float_format = '{:,.0F}'.format
+# pd.options.display.float_format = '{:,.0F}'.format
 
 for file in fullpaths:
 	df1 = pd.read_excel(file, header=2, sheet_name=None)
 	df1 = pd.concat(df1, axis=0).reset_index(drop=True)
 	df = pd.concat([df, df1], axis=0)
 
-
-tn = 1.22
 price_dict = {}
 
-#### сделать буфер
-# список из кортеж ключ - значение цена
+###### если новый размер тн - закомментировать на первый запуск
+filename = 'price.bin'
+with open(filename, 'rb') as f:
+	price_dict = pickle.load(open(filename, 'rb'))
+###########
+tn = 1.22
+df['Заказ.Клиент.Не применять топливную надбавку'] = df['Заказ.Клиент.Не применять топливную надбавку'].fillna(0)
+
+df['tn'] = df['Заказ.Клиент.Не применять топливную надбавку'].apply(lambda x: 1 if x == 1 else tn)
+
+out = ['ЭКСПРЕСС ВОЗВРАТ ДОКУМЕНТОВ', 'ЛОЖНЫЙ ВЫЗОВ', 'СКЛАД', 'ЭКСПРЕСС ГРУЗ', 'ВТОРИЧНАЯ ДОСТАВКА',
+       'СИБИРСКИЙ ЭКСПРЕСС  ДЛЯ ФИЗ.ЛИЦ', 'ВОЛЖСКИЙ ЭКСПРЕСС  СКЛАД-ДВЕРЬ ДО 0,5 КГ', 'ЭКСПРЕСС B', 'ПРАЙМ А',
+       'ЭКСПРЕСС А', 'ПРАЙМ B', 'ЭКОНОМ  СКЛАД-СКЛАД']
+
+
+def old(row):
+	row['Режим доставки'] = row['Режим доставки'].upper()
+	lst = (row['Отправитель.Адрес.Город'], row['Получатель.Адрес.Город'], row['Расчетный вес'], row['Режим доставки'])
+	if lst in price_dict.keys():
+		print(round(price_dict[lst], 2), '*')
+		return price_dict[lst]
+	else:
+		return -1
 
 
 def tarif(row):
-	row['Режим доставки'] = row['Режим доставки'].upper().strip()
+	if row['price'] == -1:
+		row['Режим доставки'] = row['Режим доставки'].upper().strip()
+		if row['Режим доставки'] in out: return 0
+		lst = (
+			row['Отправитель.Адрес.Город'], row['Получатель.Адрес.Город'], row['Расчетный вес'], row['Режим доставки'])
+		params = {'cityFrom':       row['Отправитель.Адрес.Город'], 'cityTo': row['Получатель.Адрес.Город'],
+		          'physicalWeight': row['Расчетный вес'], 'name': row['Режим доставки'], 'quantity': '1', 'width': '5',
+		          'height':         '5',
+		          'length':         '5'}
+		response = requests.get(url, params=params)
+		for i in response.json()['Result']:
+			# print(row['Режим доставки'], '+')
+			# print(i['Name'], '-')
+			if i['Name'].upper() == row['Режим доставки']:
+				print(round(i['TotalPrice'], 2))
+				price_dict[lst] = i['TotalPrice']
+				return price_dict[lst]
 
-	lst = (row['Отправитель.Адрес.Город'], row['Получатель.Адрес.Город'], row['Расчетный вес'], row['Режим доставки'])
 
-	if lst in price_dict.keys():
-		print(price_dict[lst], '*')
-		return price_dict[lst]
-
-	params = {'cityFrom':       row['Отправитель.Адрес.Город'], 'cityTo': row['Получатель.Адрес.Город'],
-	          'physicalWeight': row['Расчетный вес'], 'name': row['Режим доставки'], 'quantity': '1', 'width': '5',
-	          'height':         '5',
-	          'length':         '5'}
-	response = requests.get(url, params=params)
-
-	for i in response.json()['Result']:
-		if i['Name'].upper() == row['Режим доставки']:
-			print(round(i['TotalPrice'] * tn, 2))
-			price_dict[lst] = i['TotalPrice'] * tn
-			return price_dict[lst]
+# else:
+# 	return row['price']
 
 
 df['price'] = df.loc[:, ['Отправитель.Адрес.Город', 'Получатель.Адрес.Город', 'Расчетный вес', 'Режим доставки']].apply(
+	old, axis=1)
+
+df['price'] = df.loc[:,
+              ['Отправитель.Адрес.Город', 'Получатель.Адрес.Город', 'Расчетный вес', 'Режим доставки', 'price']].apply(
 	tarif, axis=1)
 
-print(price_dict)
 print(len(price_dict))
 
+df['price'] = df['price'] * df['tn']
 
-df = df.loc[:, ['Клиент', 'Номер отправления', 'Отправитель.Адрес.Город', 'Получатель.Адрес.Город', 'Расчетный вес', 'Режим доставки',
-                'Общая стоимость со скидкой', 'price']]
+df = df.loc[:, ['Клиент', 'Номер отправления', 'Отправитель.Адрес.Город', 'Получатель.Адрес.Город', 'Расчетный вес',
+                'Режим доставки',
+                'Общая стоимость со скидкой', 'price', 'tn']]
+
+f = open('price.bin', 'wb')
+pickle.dump(price_dict, f)
+f.close()
 
 writer = pd.ExcelWriter('цены.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='итоги', startrow=1, index=False, header=False)
