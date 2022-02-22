c90819156630f575f5ea34625864102e3e825de3
mebjus <mebjus@gmail.com>
2022-02-22 12:00:06 +0300

    v.1

--- CE/price.py
+++ CE/price.py
@@ -19,24 +19,62 @@ for file in fullpaths:
 	df1 = pd.concat(df1, axis=0).reset_index(drop=True)
 	df = pd.concat([df, df1], axis=0)
 
-# print(df.info())
-# print(df)
+
+tn = 1.22
+price_dict = {}
+
+#### сделать буфер
+# список из кортеж ключ - значение цена
 
 
 def tarif(row):
 	row['Режим доставки'] = row['Режим доставки'].upper().strip()
-	params = {'cityFrom':row['Отправитель.Адрес.Город'], 'cityTo':row['Получатель.Адрес.Город'],
-	          'physicalWeight':row['Расчетный вес'], 'name': row['Режим доставки'], 'quantity': '1', 'width': '5',
+
+	lst = (row['Отправитель.Адрес.Город'], row['Получатель.Адрес.Город'], row['Расчетный вес'], row['Режим доставки'])
+
+	if lst in price_dict.keys():
+		print(price_dict[lst], '*')
+		return price_dict[lst]
+
+	params = {'cityFrom':       row['Отправитель.Адрес.Город'], 'cityTo': row['Получатель.Адрес.Город'],
+	          'physicalWeight': row['Расчетный вес'], 'name': row['Режим доставки'], 'quantity': '1', 'width': '5',
 	          'height':         '5',
 	          'length':         '5'}
 	response = requests.get(url, params=params)
+
 	for i in response.json()['Result']:
 		if i['Name'].upper() == row['Режим доставки']:
-			return i['TotalPrice']
+			print(round(i['TotalPrice'] * tn, 2))
+			price_dict[lst] = i['TotalPrice'] * tn
+			return price_dict[lst]
 
 
-df['price'] = df.loc[:,['Отправитель.Адрес.Город', 'Получатель.Адрес.Город', 'Расчетный вес', 'Режим доставки']].apply(tarif, axis=1)
+df['price'] = df.loc[:, ['Отправитель.Адрес.Город', 'Получатель.Адрес.Город', 'Расчетный вес', 'Режим доставки']].apply(
+	tarif, axis=1)
 
-print(df['price'])
+print(price_dict)
+print(len(price_dict))
 
 
+df = df.loc[:, ['Клиент', 'Номер отправления', 'Отправитель.Адрес.Город', 'Получатель.Адрес.Город', 'Расчетный вес', 'Режим доставки',
+                'Общая стоимость со скидкой', 'price']]
+
+writer = pd.ExcelWriter('цены.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='итоги', startrow=1, index=False, header=False)
+
+workbook = writer.book
+worksheet = writer.sheets['итоги']
+
+header_format = workbook.add_format({
+	'bold':       True,
+	'text_wrap':  True,
+	'valign':     'vcenter',
+	'fg_color':   '#D7E4BC',
+	'align':      'center_across',
+	'num_format': '#,##0',
+	'border':     1})
+
+for col_num, value in enumerate(df.columns.values):
+	worksheet.write(0, col_num, value, header_format)
+
+writer.save()
